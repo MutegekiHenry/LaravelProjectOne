8a14dab46df3dc7ac0f53da45c39fa3d72b0b259
MutegekiHenry <henrymutegeki117@gmail.com>
2020-04-11 11:43:25 +0300

    Add pages and routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home','PagesController@index');
+Route::get('/about','PagesController@about');
+Route::get('/services','PagesController@services');
